32115bc9fa550eb38fd67bc8f29077446301336f
Serhii <sergbr78@gmail.com>
2023-09-19 17:43:23 +0200

    +gh deploy try
    +gh deploy try

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import  App from './components/App';
+import App from './components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* <BrowserRouter basename="https://serhiibrekhov.github.io/goit-react-hw-03-image-finder/"> */}
     <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
